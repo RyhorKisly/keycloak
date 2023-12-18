c56520087fbfdaed8568d28cffc8aaa9998c41bc
Grigoriy Kislyi <RyhorKisly@gmail.com>
2023-12-18 14:57:55 +0300

    add: method for refresh token

--- src/main/java/io/ylab/keycloak_example/services/api/IKeycloakAccessService.java
+++ src/main/java/io/ylab/keycloak_example/services/api/IKeycloakAccessService.java
@@ -3,6 +3,7 @@ package io.ylab.keycloak_example.services.api;
 import io.ylab.keycloak_example.core.dto.KeycloakServiceResponseDTO;
 import io.ylab.keycloak_example.core.dto.UserAuthorizeDTO;
 import io.ylab.keycloak_example.core.dto.UserCreateDTO;
+import org.keycloak.representations.AccessTokenResponse;
 
 /**
  * Service interface for Keycloak access operations.
@@ -21,6 +22,7 @@ public interface IKeycloakAccessService {
      * @param dto The UserAuthorizeDTO containing user authorization details
      * @return The authorization token
      */
-    String authorize(UserAuthorizeDTO dto);
+    AccessTokenResponse authorize(UserAuthorizeDTO dto);
+    AccessTokenResponse refresh(String token);
 
 }
