4a392b93b364055efdbb6af68848f023835133ab
Grigoriy Kislyi <RyhorKisly@gmail.com>
2023-12-21 15:17:55 +0300

    refactor user interaction with keycloak

--- src/main/java/io/ylab/keycloak_example/endpoints/controller/UserController.java
+++ src/main/java/io/ylab/keycloak_example/endpoints/controller/UserController.java
@@ -1,10 +1,13 @@
 package io.ylab.keycloak_example.endpoints.controller;
 
+import io.ylab.keycloak_example.core.dto.KeycloakServiceResponseDTO;
 import io.ylab.keycloak_example.core.dto.UserCreateDTO;
-import io.ylab.keycloak_example.services.api.IKeycloakService;
+import io.ylab.keycloak_example.core.dto.UserDTO;
+import io.ylab.keycloak_example.core.dto.UserUpdateDTO;
+import io.ylab.keycloak_example.services.api.KeycloakUserService;
 import lombok.RequiredArgsConstructor;
-import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,28 +29,45 @@ public class UserController {
     /**
      * Service interface for Keycloak operations related to users.
      */
-    private final IKeycloakService keycloakService;
+    private final KeycloakUserService keycloakUserService;
+
+    /**
+     * Registers a new user.
+     *
+     * @param dto The UserCreateDTO containing user details
+     * @return ResponseEntity containing the registered user's details
+     */
+    @PostMapping
+    public ResponseEntity<UserCreateDTO> create(
+            @RequestBody UserCreateDTO dto
+    ) {
+        KeycloakServiceResponseDTO<UserCreateDTO> responseDto = keycloakUserService.create(dto);
+        return new ResponseEntity<>(
+                responseDto.getDto(),
+                HttpStatusCode.valueOf(responseDto.getResponse().getStatus())
+        );
+    }
 
     /**
      * Retrieves the current user. userId gets from the token
      * @return ResponseEntity containing the current user representation
      */
     @GetMapping("/me")
-    public ResponseEntity<UserRepresentation> getUser() {
+    public ResponseEntity<UserDTO> getUser() {
         String userId = getUserIdFromToken();
-        return new ResponseEntity<>( keycloakService.getUserById(userId), HttpStatus.OK);
+        UUID uuid = UUID.fromString(userId);
+        return new ResponseEntity<>( keycloakUserService.getUserById(uuid), HttpStatus.OK);
     }
 
     /**
      * Retrieves the current user. userId gets from url
      * @return ResponseEntity containing the current user representation
      */
-    @GetMapping("/{uuid}")
-    public ResponseEntity<UserRepresentation> getUser(
-            @PathVariable UUID uuid
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUser(
+            @PathVariable UUID id
     ) {
-        //todo добавить право доступа - админ
-        return new ResponseEntity<>( keycloakService.getUserById(uuid.toString()), HttpStatus.OK);
+        return new ResponseEntity<>( keycloakUserService.getUserById(id), HttpStatus.OK);
     }
 
     /**
@@ -55,39 +75,38 @@ public class UserController {
      * @return ResponseEntity containing a list of user representations
      */
     @GetMapping
-    public ResponseEntity<List<UserRepresentation>> getUsers() {
-        return new ResponseEntity<>(keycloakService.getUsers(), HttpStatus.OK);
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        return new ResponseEntity<>(keycloakUserService.getUsers(), HttpStatus.OK);
     }
 
     /**
      * Updates a user by userId by Admin
-     * @param uuid The UUID of the user to update
-     * @param userCreateDTO The updated user data
+     * @param id The UUID of the user to update
+     * @param userUpdateDTO The updated user data
      * @return ResponseEntity containing the updated user representation
      */
-    @PutMapping("/{uuid}")
-    public ResponseEntity<UserRepresentation> updateUser(
-            @PathVariable UUID uuid,
-            @RequestBody UserCreateDTO userCreateDTO
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(
+            @PathVariable UUID id,
+            @RequestBody UserUpdateDTO userUpdateDTO
     ) {
-        //todo прописать прово доступа - админ
-        UserRepresentation user = keycloakService.updateUser(uuid.toString(), userCreateDTO);
+        UserDTO user = keycloakUserService.updateUser(id, userUpdateDTO);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     /**
      * Updates the current user. userId gets from the token
-     * @param userCreateDTO The updated user data
+     * @param userUpdateDTO The updated user data
      * @return ResponseEntity containing the updated user representation
      */
     @PutMapping("/me")
-    public ResponseEntity<UserRepresentation> updateUser(
-            @RequestBody UserCreateDTO userCreateDTO
+    public ResponseEntity<UserDTO> updateUser(
+            @RequestBody UserUpdateDTO userUpdateDTO
     ) {
-        //todo прописать прова доступа, протестить
         String userId = getUserIdFromToken();
+        UUID uuid = UUID.fromString(userId);
 
-        UserRepresentation user = keycloakService.updateUser(userId, userCreateDTO);
+        UserDTO user = keycloakUserService.updateUser(uuid, userUpdateDTO);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
@@ -96,17 +115,13 @@ public class UserController {
      * @return The user ID extracted from the token
      */
     private String getUserIdFromToken() {
-        //todo прописать прова доступа всем авторизированным
         Jwt jwt = null;
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication instanceof JwtAuthenticationToken) {
             jwt = ((JwtAuthenticationToken) authentication).getToken();
         }
 
-        //todo обработать ошибку
-        String userId = jwt.getClaim("sub");
-
-        return userId;
+        return jwt.getClaim("sub");
     }
 
 }
--- src/main/java/io/ylab/keycloak_example/services/api/KeycloakUserService.java
+++ src/main/java/io/ylab/keycloak_example/services/api/KeycloakUserService.java
@@ -2,40 +2,42 @@ package io.ylab.keycloak_example.services.api;
 
 import io.ylab.keycloak_example.core.dto.KeycloakServiceResponseDTO;
 import io.ylab.keycloak_example.core.dto.UserCreateDTO;
-import org.keycloak.representations.idm.UserRepresentation;
+import io.ylab.keycloak_example.core.dto.UserDTO;
+import io.ylab.keycloak_example.core.dto.UserUpdateDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Service interface for Keycloak operations related to users.
  */
-public interface IKeycloakService {
+public interface KeycloakUserService {
 
     /**
      * Creates a user by Admin or some special role.
      * @param dto The UserCreateDTO containing user details
      * @return KeycloakServiceResponseDTO containing the response and user DTO
      */
-    KeycloakServiceResponseDTO<UserCreateDTO> createByUser(UserCreateDTO dto);
+    KeycloakServiceResponseDTO<UserCreateDTO> create(UserCreateDTO dto);
 
     /**
      * Retrieves a user by user ID.
      * @param userId The ID of the user to retrieve
      * @return The UserRepresentation of the retrieved user
      */
-    UserRepresentation getUserById(String userId);
+    UserDTO getUserById(UUID userId);
 
     /**
      * Retrieves all users.
      * @return A list of UserRepresentation objects representing all users
      */
-    List<UserRepresentation> getUsers();
+    List<UserDTO> getUsers();
 
     /**
      * Updates a user by user ID and new user details.
      * @param userId The ID of the user to update
-     * @param dto The UserCreateDTO containing updated user details
+     * @param dto The UserUpdateDTO containing updated user details
      * @return The UserRepresentation of the updated user
      */
-    UserRepresentation updateUser(String userId, UserCreateDTO dto);
+    UserDTO updateUser(UUID userId, UserUpdateDTO dto);
 }
